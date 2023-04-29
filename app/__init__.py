42a6ff04c618e89714ce2e20dc0778ced8daf51d
oxNNxo <yankees.yes123@gmail.com>
2023-04-29 14:18:25 +0800

    init flask project

---
+++ app/__init__.py
@@ -0,0 +1,17 @@
+from flask import Flask
+
+from config import Config
+from app.datasource import db
+
+def create_app(config_class=Config):
+    app = Flask(__name__)
+    app.config.from_object(config_class)
+    db.init_app(app)
+
+    # Initialize Flask extensions here
+
+    # Register blueprints here
+    from app.main import bp as main_bp
+    app.register_blueprint(main_bp)
+
+    return app
---
+++ config.py
@@ -0,0 +1,20 @@
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class Config:
+
+	SECRET_KEY = os.getenv('SECRET_KEY')
+
+	DATABASE_HOST = os.getenv('DATABASE_HOST')
+	DATABASE_NAME = os.getenv('DATABASE_NAME')
+	DATABASE_USER = os.getenv('DATABASE_USER')
+	DATABASE_PASSWORD = os.getenv('DATABASE_PASSWORD')
+
+	SQLALCHEMY_TRACK_MODIFICATIONS = os.getenv('SQLALCHEMY_TRACK_MODIFICATIONS')
+	SQLALCHEMY_DATABASE_URI = f'postgresql://{DATABASE_USER}:{DATABASE_PASSWORD}@{DATABASE_HOST}/{DATABASE_NAME}'
+
+	HOST = os.getenv('HOST')
+	PORT = int(os.getenv('PORT', 5000))
