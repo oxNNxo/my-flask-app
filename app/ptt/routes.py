449d8bad92b5544ac72defff16c8808bb8d33248
oxNNxo <yankees.yes123@gmail.com>
2023-08-05 22:48:43 +0800

    feat(Ptt): add article filter page

--- app/ptt/routes.py
+++ app/ptt/routes.py
@@ -12,15 +12,28 @@ def index():
     return render_template('ptt/index.html')
 
 
-@bp.route('/articles')
+@bp.route('/articles', methods = ['GET', 'POST'])
 def articles():
-    page = request.args.get('page', 1, type=int)
-    per_page = request.args.get('per_page', 10, type=int)
-    articles = PttService.get_articles_paginate(page, per_page)
+    if request.method == 'GET':
+        page = request.args.get('page', 1, type=int)
+        per_page = request.args.get('per_page', 10, type=int)
+        articles = PttService.get_articles_paginate(page, per_page)
+    elif request.method == 'POST':
+        board = request.values.get('board')
+        author = request.values.get('author')
+        title = request.values.get('title')
+        startDate = request.values.get('startDate')
+        endDate = request.values.get('endDate')
+        articles = PttService.filter_article(constrains={'board':board,'author':author,'title':title,'startDate':startDate,'endDate':endDate})
     for article in articles:
         article.published = article.published.astimezone(datetime.timezone(datetime.timedelta(hours=+8))).strftime("%Y-%m-%d %H:%M:%S")
-    return render_template('ptt/ptt_article.html', ptt_article=articles)
+    return render_template('ptt/ptt_article.html', ptt_article = articles)
 
+
+@bp.route('/filter')
+def filter():
+    initialData = PttService.initial_filter()
+    return render_template('ptt/ptt_filter.html', initialData = initialData)
 
 @bp.after_request
 def log_request(response):
