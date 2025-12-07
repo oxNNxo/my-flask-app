8bd0f1784c47c82411bd5663c5e58b70eefba24d
oxNNxo <yankees.yes123@gmail.com>
2025-12-07 14:25:50 +0800

    feat(Store): add discord-bot-subscribe product page

--- app/store/routes.py
+++ app/store/routes.py
@@ -10,6 +10,10 @@ logger = logging.getLogger(__name__)
 def index():
     return render_template('store/index.html')
 
+@bp.route('/discord-bot-stock-subscribe')
+def discord_bot_stock_subscribe():
+    return render_template('store/discord_bot_subscribe.html')
+
 @bp.after_request
 def log_request(response):
     return LogService.log_request(response,logger)
